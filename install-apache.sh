1047224b36a55c9089703cc2ae605333280bce82
eoliveiralorente <31733448+eoliveiralorente@users.noreply.github.com>
2020-07-07 15:04:59 -0300

    Update install-apache.sh

--- install-apache.sh
+++ install-apache.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-apt update -y
-apt install apache2 -y
-service apache2 start
-sevice apache2 enable
-echo "Para frente MANDIC!!!!!" > /var/www/html/index.html
+echo "Para frente MANDIC!!!!!" 
